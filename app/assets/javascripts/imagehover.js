c4527568e879711cef2fe5c2ed656d39ef4be315
Kyle Ridley <kyrdly@gmail.com>
2013-09-25 17:03:17 -0500

    Added Logo and Design Changes

---
+++ app/assets/javascripts/imagehover.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//   $(".box").mouseover(function(){
+//     $(this).animate({height:"105%",width:"105%"},"fast");
+//    });
+//  $(".box").mouseout(function(){
+//     $(this).animate({height:"100%",width:"100%"},"fast");
+//    });
+// });
